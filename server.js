4c10f30736f6e6b6ef33696baf76c1c7ed5bdcf7
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-03-27 13:53:39 -0600

    heroku

--- server.js
+++ server.js
@@ -7,19 +7,29 @@ const movies = require("./movies/movies");
 
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
+
 app.use(cors());
 app.use(helmet());
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-  console.log(apiToken, authToken);
-  console.log("validated bearer token middleware");
 
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
   next();
+});
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
 
 app.get("/movie", function handleGetMovie(req, res) {
@@ -43,8 +53,6 @@ app.get("/movie", function handleGetMovie(req, res) {
   res.json(response);
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
+app.listen(PORT);
